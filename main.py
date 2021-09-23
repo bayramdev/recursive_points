d06e96aa02aff7ce82b02f5fd003fea051701a25
bayramkazik <48856944+bayramkazik@users.noreply.github.com>
2021-09-23 19:25:41 +0300

    Draw coordinate system and root point

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import constants
 import pygame
+import pygame.draw
+
+from point import Point
+from constants import *
+
+
+root_point = Point(ROOT_POINT_POSITION)
 
 
 def setup(screen):
@@ -7,8 +13,9 @@ def setup(screen):
 
 
 def update(screen):
-    screen.fill(constants.COLOR_BACKGROUND)
-    pass
+    screen.fill(COLOR_BACKGROUND)
+    draw_system(screen)
+    draw_points(screen)
 
 
 def on_mouse_button_down(pos, button):
@@ -19,9 +26,31 @@ def on_mouse_button_up(pos, button):
     pass
 
 
+def draw_points(screen):
+    scaled_pos = scale_pos(root_point)
+    pygame.draw.circle(screen, COLOR_POINT_SELECTED,
+                       scaled_pos, POINT_RADIUS_SELECTED)
+
+
+def draw_system(screen):
+    for x in range(SYSTEM_WIDTH):
+        for y in range(SYSTEM_HEIGHT):
+            pos = scale_pos((x, y))
+            pygame.draw.circle(screen, COLOR_POINT_NORMAL,
+                               pos, POINT_RADIUS_NORMAL)
+
+
+def scale_pos(pos):
+    x, y = pos
+    scaled_x = (x + 0.5) * GAP_WIDTH
+    scaled_y = (SYSTEM_HEIGHT - (y + 0.5)) * GAP_HEIGHT
+    scaled_pos = scaled_x, scaled_y
+    return scaled_pos
+
+
 def main():
     pygame.init()
-    screen = pygame.display.set_mode(constants.SIZE)
+    screen = pygame.display.set_mode(SIZE)
     setup(screen)
 
     running = True
