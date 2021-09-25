068e7555968538644282c76edf748d9ff222796e
bayramkazik <48856944+bayramkazik@users.noreply.github.com>
2021-09-25 16:21:16 +0300

    Remove overflow of coordinate system lines

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from constants import *
 
 root_point = Point(0, 0)
 
+
 child_generators = [
     lambda x, y: (x + 7, y),       # 7 up
     lambda x, y: (x - 13, y - 6),  # 13 left, 6 down
@@ -93,13 +94,13 @@ def on_mouse_button_up(pos, button):
 def draw_points(screen):
     draw_children(screen, root_point)
 
-    scaled_pos = scale_pos(root_point)
+    scaled_pos = scale_pos(root_point.x, root_point.y)
     pygame.draw.circle(screen, COLOR_POINT_SELECTED,
                        scaled_pos, POINT_RADIUS_SELECTED)
 
 
 def draw_children(screen, point):
-    scaled_pos = scale_pos(point)
+    scaled_pos = scale_pos(point.x, point.y)
     pygame.draw.circle(screen, COLOR_POINT_SELECTED,
                        scaled_pos, POINT_RADIUS_NORMAL)
 
@@ -110,31 +111,27 @@ def draw_children(screen, point):
 def draw_system_points(screen):
     for x in range(SYSTEM_WIDTH):
         for y in range(SYSTEM_HEIGHT):
-            pos = scale_pos((x, y))
+            pos = scale_pos(x, y)
             pygame.draw.circle(screen, COLOR_POINT_NORMAL,
                                pos, POINT_RADIUS_NORMAL)
 
 
 def draw_system_lines(screen):
     for x in range(SYSTEM_WIDTH):
-        scaled_x = (x + 0.5) * GAP_WIDTH
-        start = scaled_x, 0
-        end = scaled_x, HEIGHT
+        start = scale_pos(x, 0)
+        end = scale_pos(x, SYSTEM_HEIGHT - 1)
         pygame.draw.line(screen, COLOR_LINE_NORMAL, start, end)
 
     for y in range(SYSTEM_HEIGHT):
-        scaled_y = (y + 0.5) * GAP_WIDTH
-        start = 0, scaled_y
-        end = WIDTH, scaled_y
+        start = scale_pos(0, y)
+        end = scale_pos(SYSTEM_WIDTH - 1, y)
         pygame.draw.line(screen, COLOR_LINE_NORMAL, start, end)
 
 
-def scale_pos(pos):
-    x, y = pos
+def scale_pos(x, y):
     scaled_x = (x + 0.5) * GAP_WIDTH
     scaled_y = (SYSTEM_HEIGHT - (y + 0.5)) * GAP_HEIGHT
-    scaled_pos = scaled_x, scaled_y
-    return scaled_pos
+    return scaled_x, scaled_y
 
 
 def create_children(point: Point, lookup_table):
