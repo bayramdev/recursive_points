333b3b0a23deed84ba6a4f16df6bb3d31d7bb0a6
bayramkazik <48856944+bayramkazik@users.noreply.github.com>
2021-09-25 20:36:24 +0300

    Add type hinting for function arguments

--- main.py
+++ main.py
@@ -5,14 +5,23 @@
 
 import os
 import sys
+from typing import List, Tuple
 
 import pygame
-import pygame.draw
 import pygame.display
+import pygame.draw
+import pygame.event
 import pygame.image
+from pygame.surface import Surface
 
 from point import Point
 from constants import *
+
+
+# TYPES
+
+OrderedPair = Tuple[int, int]
+AreaMatrix = List[List[int]]
 
 
 # GLOBALS
@@ -67,7 +76,7 @@ def init():
     pygame.display.set_caption(WINDOW_TITLE)
 
 
-def setup(screen):
+def setup(screen: Surface):
     lookup_table = [[0 for _ in range(AREA_WIDTH)] for _ in range(AREA_HEIGHT)]
     init_child_points(root_point, lookup_table)
 
@@ -78,26 +87,26 @@ def setup(screen):
     draw_root_point(screen)
 
 
-def update(screen):
+def update(screen: Surface):
     pass
 
 
-def on_mouse_button_down(position, button):
+def on_mouse_button_down(position: OrderedPair, button: int):
     pass
 
 
-def on_mouse_button_up(position, button):
+def on_mouse_button_up(position: OrderedPair, button: int):
     pass
 
 
 # DRAW HELPERS
 
-def draw_root_point(screen):
+def draw_root_point(screen: Surface):
     scaled_pos = scale_position(root_point.x, root_point.y)
     pygame.draw.circle(screen, ROOT_POINT_COLOR, scaled_pos, POINT_RADIUS)
 
 
-def draw_child_points(screen, point):
+def draw_child_points(screen: Surface, point: Point):
     scaled_pos = scale_position(point.x, point.y)
     pygame.draw.circle(screen, CHILD_POINT_COLOR, scaled_pos, POINT_RADIUS)
 
@@ -105,14 +114,14 @@ def draw_child_points(screen, point):
         draw_child_points(screen, child)
 
 
-def draw_grid_points(screen):
+def draw_grid_points(screen: Surface):
     for x in range(AREA_WIDTH):
         for y in range(AREA_HEIGHT):
             pos = scale_position(x, y)
             pygame.draw.circle(screen, GRID_POINT_COLOR, pos, POINT_RADIUS)
 
 
-def draw_grid_lines(screen):
+def draw_grid_lines(screen: Surface):
     for x in range(AREA_WIDTH):
         start = scale_position(x, 0)
         end = scale_position(x, AREA_WIDTH - 1)
@@ -126,13 +135,13 @@ def draw_grid_lines(screen):
 
 # UTILS
 
-def scale_position(x, y):
+def scale_position(x: int, y: int) -> OrderedPair:
     scaled_x = (x + 0.5) * GAP_WIDTH
     scaled_y = (AREA_HEIGHT - (y + 0.5)) * GAP_HEIGHT
     return scaled_x, scaled_y
 
 
-def init_child_points(point: Point, lookup_table):
+def init_child_points(point: Point, lookup_table: AreaMatrix):
     for child_generator in child_functions:
         child = Point(child_generator(point.x, point.y))
         x, y = int(child.x), int(child.y)
@@ -143,7 +152,7 @@ def init_child_points(point: Point, lookup_table):
             point.children.append(child)
 
 
-def is_in_boundry(x, y) -> bool:
+def is_in_boundry(x: int, y: int) -> bool:
     return 0 <= x < AREA_WIDTH and 0 <= y < AREA_HEIGHT
 
 
--- point.py
+++ point.py
@@ -2,7 +2,7 @@ import pygame.math
 
 
 class Point(pygame.math.Vector2):
-    def __init__(self, x, y=0, children: list = None):
+    def __init__(self, x: int, y: int = 0, children: list = None):
         super().__init__(x, y)
         self.children = children
         if self.children == None:
